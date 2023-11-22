d48e0b7698b28d3bfbf21c9e05a8f9eeefd2b557
Matthew Leibowitz <mattleibow@live.com>
2023-11-22 04:14:35 +0200

    Add WASM

--- BlazorAllTheThings/BlazorAllTheThings/Program.cs
+++ BlazorAllTheThings/BlazorAllTheThings/Program.cs
@@ -4,14 +4,23 @@ using BlazorAllTheThings.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+// TODO: 07 [Render Mode] Add `.AddInteractiveServerComponents()` to enable interactive server-side components
+// TODO: 10 [Render Mode] Add `.AddInteractiveWebAssemblyComponents()` to enable interactive wasm components
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddSingleton<WeatherService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    // TODO: 12 [Debugging] Add `app.UseWebAssemblyDebugging()` to enable interactive wasm components
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -23,6 +32,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+// TODO: 08 [Render Mode] Add `.AddInteractiveServerRenderMode()` to enable interactive server-side components
+// TODO: 11 [Render Mode] Add `.AddInteractiveWebAssemblyRenderMode()` to enable interactive wasm components
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    ;
 
 app.Run();
